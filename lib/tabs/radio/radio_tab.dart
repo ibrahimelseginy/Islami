8be286f687c6a859084a6d4c3be325a5b898f316
ibrahimelseginy <ibrahimelseginy02@gmail.com>
2025-01-20 01:10:33 +0000

    finish Quran tab

--- lib/tabs/radio/radio_tab.dart
+++ lib/tabs/radio/radio_tab.dart
@@ -5,8 +5,48 @@ class RadioTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // color: Colors.orange,
-        );
+    return Column(
+      children: [
+        SizedBox(
+          height: 100,
+        ),
+        Image(
+          image: AssetImage('assets/images/radio_image.png'),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'إذاعة القرآن الكريم',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Icon(
+            Icons.skip_next_sharp,
+            color: Theme.of(context).primaryColor,
+            size: 30,
+          ),
+          SizedBox(
+            width: 30,
+          ),
+          Icon(
+            Icons.play_arrow_rounded,
+            color: Theme.of(context).primaryColor,
+            size: 50,
+          ),
+          SizedBox(
+            width: 30,
+          ),
+          Icon(
+            Icons.skip_previous_sharp,
+            color: Theme.of(context).primaryColor,
+            size: 30,
+          ),
+        ])
+      ],
+    );
   }
 }
--- lib/tabs/sebha/sebha_tab.dart
+++ lib/tabs/sebha/sebha_tab.dart
@@ -5,8 +5,58 @@ class SebhaTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // color: Colors.red,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        const Column(
+          children: [
+            Image(
+              height: 90,
+              width: 170,
+              image: AssetImage('assets/images/head_sebha_logo.png'),
+            ),
+            Image(
+              height: 200,
+              width: 280,
+              image: AssetImage(
+                'assets/images/body_sebha_logo.png',
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          'عدد التسبيحات',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        Container(
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              color: Theme.of(context).primaryColor),
+          child: Text(
+            '33',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              color: Theme.of(context).primaryColor),
+          child: Text(
+            'سبحان الله',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+        )
+      ],
     );
   }
 }
